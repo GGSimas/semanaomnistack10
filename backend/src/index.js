aa4e8ed1c7cca2d6cc7d61fb2d18a01c2a410ce4
Gabriel Simões <simoes.gabrielm@gmail.com>
2020-01-16 00:29:17 -0300

    adicionando a parte web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const route = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 //   "mongodb+srv://week10:week10@cluster0-2naru.mongodb.net/week10?retryWrites=true&w=majority",
 const app = express();
@@ -10,6 +11,7 @@ mongoose.connect("mongodb://localhost:27017/week10", {
   useCreateIndex: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(route);
 
